3faa51a183f352c90c9b3b01eb39829a0264504b
HananAwwad <eng.hanan.awwad@gmail.com>
2020-04-20 10:26:39 +0300

    minPathSum

--- src/com/hanan/Solution2.java
+++ src/com/hanan/Solution2.java
@@ -57,35 +57,44 @@ public class Solution2 {
                 {4, 2, 1}
         };
 
-        System.out.println("Number of islands is:::::   " + new Solution2().numIslands(M));
+        System.out.println("Number of islands is:::::   " + new Solution2().minPathSum(s));
     }
 
 
     public int minPathSum(int[][] grid) {
-        //int []
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
 
+        if (grid == null || grid.length == 0)
+            return 0;
 
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+
+        // initialize top row
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+
+        // initialize left column
+        for (int j = 1; j < m; j++) {
+            dp[j][0] = dp[j - 1][0] + grid[j][0];
+        }
+
+        // fill up the dp table
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (dp[i - 1][j] > dp[i][j - 1]) {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                }
             }
         }
-        return 0;
-    }
-    public int getRightPathLength(int[][] grid, int i , int j){
-                // i is constant and j starting from j and increament
-        int pathLenght = 0 ;
-        for (int jj = j ; jj< grid[i].length ; jj++){
-            pathLenght += grid[i][jj];
-        }
-        return pathLenght;
-    }
-    public int getDownPathLength(int[][] grid, int i , int j){
-        // i is constant and j starting from j and increament
-        int pathLenght = 0 ;
-        for (int jj = j ; jj< grid.length ; jj++){
-            pathLenght += grid[i][jj];
-        }
-        return pathLenght;
+
+        return dp[m - 1][n - 1];
+
     }
 
     public int numIslands(char[][] grid) {
