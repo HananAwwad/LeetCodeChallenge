430bad0e701eaf539570ffc0662c1d20c424b4fc
HananAwwad <eng.hanan.awwad@gmail.com>
2020-05-06 12:37:44 +0300

    first unique char

--- src/com/hanan/may/Solution.java
+++ src/com/hanan/may/Solution.java
@@ -7,8 +7,24 @@ public class Solution {
 
     public static void main(String s[]) {
        // System.out.println(new Solution().firstBadVersion(5));
-        System.out.println(new Solution().findComplement(2));
+       // System.out.println(new Solution().findComplement(2));
+        System.out.printf("unique char "+new Solution().firstUniqChar( "loveleetcode"));
 
+    }
+
+    public int firstUniqChar(String s) {
+
+        int [] map = new int[26];
+        for (char c :s.toCharArray()){
+            map[c-'a']++;
+        }
+        for (int i = 0 ; i< s.length() ; i++ ){
+            char c = s.charAt(i);
+            if (map[c-'a'] ==  1){
+               return i;
+            }
+        }
+        return -1;
     }
     public int findComplement(int num) {
 
