6358dfe22504861edd7af93d52500314814dcc70
HananAwwad <eng.hanan.awwad@gmail.com>
2020-05-10 18:57:50 +0300

    find judge

--- src/com/hanan/may/Solution.java
+++ src/com/hanan/may/Solution.java
@@ -13,7 +13,24 @@ public class Solution {
         // System.out.printf("unique char " + new Solution().firstUniqChar("loveleetcode"));
         // System.out.println(new Solution().majorityElementBetterSol(new int[]{3, 2, 3}));
        // System.out.println(new Solution().checkStraightLine(new int[][]{{-4, -3}, {1, 0}, {3, -1}, {0, -1}, {-5, 2}}));
-        System.out.println(new Solution().isPerfectSquare(14));
+        //System.out.println(new Solution().isPerfectSquare(14));
+        System.out.println(new Solution().findJudge(4,new int[][]{{1,3},{1,4},{2,3},{2,4},{4,3}}));
+    }
+
+    public int findJudge(int N, int[][] trust) {
+
+        int [] inDegree = new int[N+1];
+        int [] outDegree = new int[N+1];
+        for (int i=0; i < trust.length ; i++){
+            outDegree[trust[i][0]]++;
+            inDegree[trust[i][1]]++;
+
+        }
+        for (int i = 1 ; i <= N ; i++){
+            if (outDegree[i] == 0 && inDegree[i] == (N-1))
+                return i;
+        }
+        return -1;
     }
 
     public boolean isPerfectSquare(int num) {
