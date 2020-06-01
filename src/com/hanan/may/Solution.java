779bc07db4d54f3fd3898d97e11dcb90a71ec7df
HananAwwad <eng.hanan.awwad@gmail.com>
2020-06-01 13:01:25 +0300

    minDistance

--- src/com/hanan/may/Solution.java
+++ src/com/hanan/may/Solution.java
@@ -37,9 +37,39 @@ public class Solution {
 //            System.out.println(param_1);
 //        }
 //        System.out.println(" last value " + obj.next(10));
-    //        System.out.println(new Solution().maxUncrossedLines(new int[]{2, 5, 1, 2, 5}, new int[]{10, 5, 2, 1, 5, 2}));
-        System.out.println(new Solution().kClosest(new int[][]{{3,3},{5,-1},{-2,4}}, 2));
+        //        System.out.println(new Solution().maxUncrossedLines(new int[]{2, 5, 1, 2, 5}, new int[]{10, 5, 2, 1, 5, 2}));
+        System.out.println(new Solution().kClosest(new int[][]{{3, 3}, {5, -1}, {-2, 4}}, 2));
 
+    }
+
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i < word1.length() + 1; i++) {
+            for (int j = 0; j < word2.length() + 1; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return ed(word1, word2, 0, 0, dp);
+    }
+
+    public int ed(String word1, String word2, int i, int j, int[][] dp) {
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (i == word1.length() || j == word2.length()) {
+            return dp[i][j] = (i == word1.length()) ? word2.length() - j : word1.length() - i;
+
+        }
+        if (word1.charAt(i) == word2.charAt(j)) {
+            return dp[i][j] = ed(word1, word2, i + 1, j + 1, dp);
+        } else {
+            return dp[i][j] = 1 + Math.min(
+                    ed(word1, word2, i, j + 1, dp),
+                    Math.min(ed(word1, word2, i + 1, j, dp),
+                            ed(word1, word2, i + 1, j + 1, dp))
+            );
+        }
     }
 
     public int[][] kClosest(int[][] points, int K) {
