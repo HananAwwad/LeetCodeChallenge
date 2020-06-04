c4c04681f1b8716e94f5e69e92a81efb1ddedbee
HananAwwad <eng.hanan.awwad@gmail.com>
2020-06-05 00:34:26 +0300

    two city scheduling min cost

--- src/com/hanan/june/JuneSolution.java
+++ src/com/hanan/june/JuneSolution.java
@@ -1,17 +1,35 @@
 package com.hanan.june;
 
+import java.util.Arrays;
+
 public class JuneSolution {
 
     public static void main(String[] aa) {
-        int[] a = new int[]{4, 2, 7, 1, 3, 6, 9};
-        for (int i = 0; i < a.length; i++) {
+//        int[] a = new int[]{4, 2, 7, 1, 3, 6, 9};
+//        for (int i = 0; i < a.length; i++) {
+//
+//        }
+//        char[] s = new char[]{'h','e','l','l','o'};
+//        new JuneSolution().reverseString(s);
+//        System.out.println(s);
 
-        }
-        char[] s = new char[]{'h','e','l','l','o'};
-        new JuneSolution().reverseString(s);
-        System.out.println(s);
+        System.out.println(new JuneSolution().twoCitySchedCost(new int[][]{{10,20},{30,200},{400,50},{30,20}}));
     }
 
+    public int twoCitySchedCost(int[][] costs) {
+        int N = costs.length/2;
+        int[] refund = new int[N * 2];
+        int minCost = 0, index = 0;
+        for(int[] cost : costs){
+            refund[index++] = cost[1] - cost[0];
+            minCost += cost[0];
+        }
+        Arrays.sort(refund);
+        for(int i = 0; i < N; i++){
+            minCost += refund[i];
+        }
+        return minCost;
+    }
     public void reverseString(char[] s) {
         int left = 0;
         int right = s.length - 1;
