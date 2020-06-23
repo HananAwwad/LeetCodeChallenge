4ef429229f29b99e61c1bccafc6a21a673e75a76
HananAwwad <eng.hanan.awwad@gmail.com>
2020-06-23 15:39:55 +0300

    min knight health

--- src/com/hanan/june/JuneSolution.java
+++ src/com/hanan/june/JuneSolution.java
@@ -38,10 +38,36 @@ public class JuneSolution {
         // System.out.println(new JuneSolution().validIPAddress("2001:0db8:85a3:0000:0000:8a2e:0370:7334"));
         //System.out.println(new JuneSolution().longestDupSubstring(""));
         //System.out.println(new JuneSolution().getPermutation(3, 3));
-        System.out.println(new JuneSolution().singleNumber(new int[]{1,1,2,2,2,99,99,99}));
+       // System.out.println(new JuneSolution().singleNumber(new int[]{1,1,2,2,2,99,99,99}));
+        System.out.println(new JuneSolution().calculateMinimumHP(new int[][]{{-2,-3,3},{-5,-10,1},{10,30,-5}}));
     }
 
+    public int calculateMinimumHP(int[][] dungeon) {
+        int n = dungeon.length;
+        int m = dungeon[0].length;
+        int[][] dp = new int[n][m];
 
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                if (i + 1 == n && j + 1 == m) {
+                    dp[i][j] = Math.max(1 - dungeon[i][j], 1);
+                    continue;
+                }
+
+                if (i + 1 == n) {
+                    dp[i][j] = Math.max(dp[i][j + 1] - dungeon[i][j], 1);
+                } else if (j + 1 == m) {
+                    dp[i][j] = Math.max(dp[i + 1][j] - dungeon[i][j], 1);
+                } else {
+                    int right = Math.max(dp[i][j + 1] - dungeon[i][j], 1);
+                    int down = Math.max(dp[i + 1][j] - dungeon[i][j], 1);
+                    dp[i][j] = Math.min(right, down);
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
     public int singleNumber(int[] nums) {
         Arrays.sort(nums);
 
