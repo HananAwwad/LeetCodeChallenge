334e3826cb8fa9ed7173c6faa5e89c6b422d3f35
HananAwwad <eng.hanan.awwad@gmail.com>
2020-06-30 12:49:12 +0300

    uniquePaths

--- src/com/hanan/june/JuneSolution.java
+++ src/com/hanan/june/JuneSolution.java
@@ -45,6 +45,41 @@ public class JuneSolution {
         System.out.println(new JuneSolution().numTrees(4));
     }
 
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int dp[][] = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][0] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+                    }
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+
+    }
+
+    public int uniquePaths(int m, int n) {
+        int dp[][] = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
     Map<String, PriorityQueue<String>> graph;
     LinkedList res;
 
