e01faa31a61af73a400064b737418b58fa299fbe
HananAwwad <eng.hanan.awwad@gmail.com>
2020-07-04 15:14:51 +0300

    prisonAfterNDays

--- src/com/hanan/JulySolution.java
+++ src/com/hanan/JulySolution.java
@@ -2,7 +2,22 @@ package com.hanan;
 
 public class JulySolution {
     public static void main(String[] aa) {
-        System.out.println(new JulySolution().arrangeCoins(5));
+        //System.out.println(new JulySolution().arrangeCoins(5));
+        System.out.println(new JulySolution().prisonAfterNDays(new int[]{0, 1, 0, 1, 1, 0, 0, 1}, 50));
+    }
+
+    public int[] prisonAfterNDays(int[] cells, int N) {
+        N = (N - 1) % 14 + 1;
+        for (int i = 0; i < N; i++)
+            cells = nextDayState(cells);
+        return cells;
+    }
+
+    private int[] nextDayState(int[] cells) {
+        int[] next = new int[cells.length];
+        for (int i = 1; i < cells.length - 1; i++)
+            next[i] = (cells[i - 1] == cells[i + 1]) ? 1 : 0;
+        return next;
     }
 
     public int arrangeCoins(int n) {
