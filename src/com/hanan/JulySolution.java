2514a8cf7380496f917fd63d868723e9da9c2103
HananAwwad <eng.hanan.awwad@gmail.com>
2020-07-18 11:52:54 +0300

    changes

--- src/com/hanan/JulySolution.java
+++ src/com/hanan/JulySolution.java
@@ -10,6 +10,54 @@ public class JulySolution {
         //System.out.println(Integer.parseInt("00000010100101000001111010011100"));
         // System.out.println(new JulySolution().reverseBits(Integer.parseInt("00000010100101000001111010011100")));
         System.out.println(new JulySolution().ArrayChallenge(new int[]{1, 1, 2, 10, 3, 1, 12}));
+
+    }
+
+    public boolean check(TreeNode p, TreeNode q) {
+        // p and q are null
+        if (p == null && q == null) return true;
+        // one of p and q is null
+        if (q == null || p == null) return false;
+        if (p.val != q.val) return false;
+        return true;
+    }
+
+    public boolean isSameTree1(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (!check(p, q)) return false;
+
+        // init deques
+        ArrayDeque<TreeNode> deqP = new ArrayDeque<TreeNode>();
+        ArrayDeque<TreeNode> deqQ = new ArrayDeque<TreeNode>();
+        deqP.addLast(p);
+        deqQ.addLast(q);
+
+        while (!deqP.isEmpty()) {
+            p = deqP.removeFirst();
+            q = deqQ.removeFirst();
+
+            if (!check(p, q)) return false;
+
+            if (p != null) {
+
+                // in Java nulls are not allowed in Deque
+
+                if (!check(p.left, q.left)) return false;
+
+                if (p.left != null) {
+                    deqP.addLast(p.left);
+                    deqQ.addLast(q.left);
+                }
+
+                if (!check(p.right, q.right)) return false;
+
+                if (p.right != null) {
+                    deqP.addLast(p.right);
+                    deqQ.addLast(q.right);
+                }
+            }
+        }
+        return true;
     }
 
 
@@ -25,11 +73,10 @@ public class JulySolution {
 
 
     public static int ArrayChallenge(int[] arr) {
+
+        int sum = (Arrays.stream(arr).sum()) * 2;
         Arrays.sort(arr);
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
-        }
+        //Arrays.stream(arr).asDoubleStream()
         int doubleSum = sum * sum;
         int prevelement = arr[0];
         for (int i = 1; i < arr.length; i++) {
