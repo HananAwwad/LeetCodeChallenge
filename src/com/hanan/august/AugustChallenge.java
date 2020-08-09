0d436f78387c94f57d2ba1ecb69c65620092155e
HananAwwad <eng.hanan.awwad@gmail.com>
2020-08-09 10:31:10 +0300

    path sum in tree

--- src/com/hanan/august/AugustChallenge.java
+++ src/com/hanan/august/AugustChallenge.java
@@ -1,5 +1,12 @@
 package com.hanan.august;
 
+
+import com.hanan.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class AugustChallenge {
 
     public static void main(String[] aa) {
@@ -7,13 +14,67 @@ public class AugustChallenge {
 //        System.out.println("Capital.." + new AugustChallenge().detectCapitalUse("Capital"));
 //        System.out.println("small...." + new AugustChallenge().detectCapitalUse("small"));
 // Your WordDictionary object will be instantiated and called as such:
-        WordDictionary obj = new WordDictionary();
-        obj.addWord("bad");
-        obj.addWord("dad");
-        obj.addWord("mad");
+//        WordDictionary obj = new WordDictionary();
+//        obj.addWord("bad");
+//        obj.addWord("dad");
+//        obj.addWord("mad");
+//
+//        boolean param_2 = obj.search("b..");
+//        System.out.println(param_2);
+    }
 
-        boolean param_2 = obj.search("b..");
-        System.out.println(param_2);
+    public int pathSumIII(TreeNode root, int sum) {
+        if (root == null)
+            return 0;
+
+        return helper(root, sum) + pathSumIII(root.left, sum) + pathSumIII(root.right, sum);
+    }
+
+    private int helper(TreeNode node, int sum) {
+        if (node == null)
+            return 0;
+        else
+            return (node.val == sum ? 1 : 0)
+                    + helper(node.left, sum - node.val)
+                    + helper(node.right, sum - node.val);
+    }
+
+
+    List<List<Integer>> ans = new ArrayList<>();
+
+    public List<List<Integer>> pathSumII(TreeNode root, int sum) {
+        fillPaths(root, sum, sum, new ArrayList());
+        return ans.s;
+    }
+
+    public void fillPaths(TreeNode root, int currentSum, int sum, List path) {
+        if (root == null)
+            return;
+        currentSum -= root.val;
+        path.add(root.val);
+        if (currentSum == 0 && root.left == null && root.right == null) {
+            ans.add(new LinkedList<>(path));
+            path.remove(path.size() - 1);
+            return;
+        }
+        fillPaths(root.left, currentSum, sum, path);
+        fillPaths(root.right, currentSum, sum, path);
+        path.remove(path.size() - 1);
+
+    }
+
+
+    public boolean hasPathSumI(TreeNode root, int sum) {
+        return hasPathSumRecursive(root, sum);
+    }
+
+    public boolean hasPathSumRecursive(TreeNode root, int sum) {
+        if (root == null)
+            return false;
+        sum -= root.val;
+        if (sum == 0 && root.left == null && root.right == null)
+            return true;
+        return hasPathSumRecursive(root.left, sum) || hasPathSumRecursive(root.right, sum);
     }
 
     public boolean detectCapitalUse(String word) {
