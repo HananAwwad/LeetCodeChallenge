6a71ae1a14a73b1af9033829f6bd1d83bf5e473d
HananAwwad <eng.hanan.awwad@gmail.com>
2020-09-08 19:13:48 +0300

    wordPattern

--- src/com/hanan/Sep/SepChallenge.java
+++ src/com/hanan/Sep/SepChallenge.java
@@ -1,12 +1,77 @@
 package com.hanan.Sep;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SepChallenge {
     public static void main(String a[]) {
-        System.out.println(new SepChallenge().largestOverlap(new int[][]{{1, 1, 0},
-                {0, 1, 0},
-                {0, 1, 0}}, new int[][]{{0, 0, 0},
-                {0, 1, 1},
-                {0, 0, 1}}));
+//        System.out.println(new SepChallenge().largestOverlap(new int[][]{{1, 1, 0},
+//                {0, 1, 0},
+//                {0, 1, 0}}, new int[][]{{0, 0, 0},
+//                {0, 1, 1},
+//                {0, 0, 1}}));
+        //System.out.println(new SepChallenge().wordPattern("abba", "dog cat cat dog"));
+        System.out.println(new SepChallenge().isIsomorphic("egg", "add"));
+
+    }
+
+    public boolean isIsomorphic(String s, String t) {
+
+        int n = s.length(), i = 0;
+        if (n != t.length())
+            return false;
+
+        Map<Character, Character> sMap = new HashMap();
+        Map<Character, Character> tMap = new HashMap();
+
+        while (i < n) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+            if (sMap.containsKey(sChar) != tMap.containsKey(tChar))
+                return false;
+
+            if (sMap.containsKey(sChar)) {
+                if (sMap.get(sChar) != tChar || tMap.get(tChar) != sChar)
+                    return false;
+            } else {
+                sMap.put(sChar, tChar);
+                tMap.put(tChar, sChar);
+            }
+
+            i++;
+        }
+
+        return i == n;
+    }
+
+    public boolean wordPattern(String pattern, String str) {
+        String[] words = str.split(" ");
+
+        int n = pattern.length(), i = 0;
+        if (n != words.length)
+            return false;
+
+        Map<Character, String> chrarMap = new HashMap();
+        Map<String, Character> stringMap = new HashMap();
+
+        while (i < n) {
+            char c = pattern.charAt(i);
+            String word = words[i];
+            if (chrarMap.containsKey(c) != stringMap.containsKey(word))
+                return false;
+
+            if (chrarMap.containsKey(c)) {
+                if (!chrarMap.get(c).equals(word) || stringMap.get(word) != c)
+                    return false;
+            } else {
+                chrarMap.put(c, word);
+                stringMap.put(word, c);
+            }
+
+            i++;
+        }
+
+        return i == n;
     }
 
 
