8002ec62ee363732fa68992f11b118fba093864b
HananAwwad <eng.hanan.awwad@gmail.com>
2020-10-09 09:48:52 +0300

    binary search

--- src/com/hanan/Sep/SepChallenge.java
+++ src/com/hanan/Sep/SepChallenge.java
@@ -49,6 +49,22 @@ public class SepChallenge {
         System.out.println(new SepChallenge().bitwiseComplement(5));
     }
 
+    public int search(int[] nums, int target) {
+
+        int pivot, left = 0, right = nums.length - 1;
+        while (left <= right) {
+            pivot = left + (right - left) / 2;
+
+            if (nums[pivot] == target)
+                return pivot;
+
+            if (target < nums[pivot]) right = pivot - 1;
+
+            else left = pivot + 1;
+        }
+        return -1;
+    }
+
     public ListNode rotateRight(ListNode head, int k) {
         if (head == null || k == 0)
             return head;
