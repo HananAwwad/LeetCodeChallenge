8b7b45ecfa0878bfd321f7a40c9b83819ee8a940
HananAwwad <eng.hanan.awwad@gmail.com>
2020-10-28 11:32:51 +0200

    champagneTower

--- src/com/hanan/OctChallenge.java
+++ src/com/hanan/OctChallenge.java
@@ -9,7 +9,26 @@ public class OctChallenge {
         // System.out.println(new OctChallenge().maxProfit(1, new int[]{3, 2, 6, 5, 0, 3}));
         // System.out.println(new OctChallenge().minDominoRotations(new int[]{2, 1, 2, 4, 2, 2}, new int[]{5, 2, 6, 2, 3, 2}));
         //    System.out.println(new OctChallenge().find132pattern(new int[]{3, 1, 4, 2}));
-        System.out.println(new OctChallenge().bagOfTokensScore(new int[]{100, 200, 300, 400}, 200));
+        //    System.out.println(new OctChallenge().bagOfTokensScore(new int[]{100, 200, 300, 400}, 200));
+        System.out.println(new OctChallenge().champagneTower(2 , 1  , 1));
+    }
+
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        if (poured == 0)
+            return 0;
+        List<Double> l = new ArrayList();
+        l.add((double) poured);
+        while (query_row -- > 0) {
+            List<Double> temp = new ArrayList();
+            temp.add(Math.max((l.get(0) - 1) / 2, 0));
+            for (int i = 1; i < l.size(); i++) {
+                temp.add(Math.max((l.get(i - 1) - 1) / 2, 0) + Math.max((l.get(i) -  1) / 2, 0));
+            }
+            temp.add(temp.get(0));
+            l = new ArrayList<>(temp);
+
+        }
+        return Math.min(1, l.get(query_glass));
     }
 
     public int bagOfTokensScore(int[] tokens, int P) {
