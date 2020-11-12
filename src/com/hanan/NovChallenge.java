5a32ee5d7ba6a5b0e3f311bdc1f11aff69c36eaa
HananAwwad <eng.hanan.awwad@gmail.com>
2020-11-12 14:31:58 +0200

    permuteUnique

--- src/com/hanan/NovChallenge.java
+++ src/com/hanan/NovChallenge.java
@@ -19,10 +19,59 @@ public class NovChallenge {
         // System.out.println(new NovChallenge().findMaxConsecutiveOnes(new int[]{1}));
         //System.out.println(new NovChallenge().findTilt(new TreeNode(5)));
         // System.out.println(new NovChallenge().maxAncestorDiff(new TreeNode(5)));
-      //  System.out.println(new NovChallenge().flipAndInvertImage(new int[][]{{1, 1, 0, 0}, {1, 0, 0, 1}, {0, 1, 1, 1}, {1, 0, 1, 0}}));
-        System.out.println(new NovChallenge().validSquare(new int[]{0,0},new int[]{1,1},new int[]{0,1},new int[]{1,0}));
+        //  System.out.println(new NovChallenge().flipAndInvertImage(new int[][]{{1, 1, 0, 0}, {1, 0, 0, 1}, {0, 1, 1, 1}, {1, 0, 1, 0}}));
+        //   System.out.println(new NovChallenge().validSquare(new int[]{0,0},new int[]{1,1},new int[]{0,1},new int[]{1,0}));
+        System.out.println(new NovChallenge().permuteUnique(new int[]{1, 2, 3}));
 
     }
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+
+        // count the occurrence of each number
+        HashMap<Integer, Integer> counter = new HashMap<>();
+        for (int num : nums) {
+            if (!counter.containsKey(num))
+                counter.put(num, 0);
+            counter.put(num, counter.get(num) + 1);
+        }
+
+        LinkedList<Integer> comb = new LinkedList<>();
+        this.backtrack(comb, nums.length, counter, results);
+        return results;
+    }
+
+    protected void backtrack(
+            LinkedList<Integer> comb,
+            Integer N,
+            HashMap<Integer, Integer> counter,
+            List<List<Integer>> results) {
+
+        if (comb.size() == N) {
+            // make a deep copy of the resulting permutation,
+            // since the permutation would be backtracked later.
+            results.add(new ArrayList<Integer>(comb));
+            return;
+        }
+
+        for (Map.Entry<Integer, Integer> entry : counter.entrySet()) {
+            Integer num = entry.getKey();
+            Integer count = entry.getValue();
+            if (count == 0)
+                continue;
+            // add this number into the current combination
+            comb.addLast(num);
+            counter.put(num, count - 1);
+
+            // continue the exploration
+            backtrack(comb, N, counter, results);
+
+            // revert the choice for the next exploration
+            comb.removeLast();
+            counter.put(num, count);
+        }
+    }
+
 
     public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
         Set<Integer> set = new HashSet<>();
