b6a4c017ce0606dca3bf6d89cbbceb166599a6d1
HananAwwad <eng.hanan.awwad@gmail.com>
2020-12-21 11:54:26 +0200

    decodeAtIndex

--- src/com/hanan/DecChallenge.java
+++ src/com/hanan/DecChallenge.java
@@ -8,7 +8,37 @@ public class DecChallenge {
     public static void main(String[] aa) {
         //  System.out.println(new DecChallenge().partition(""));
         //  System.out.println(new DecChallenge().sortedSquares(new int[]{-4, -1, 0, 3, 10}));
-        System.out.println(new DecChallenge().isValidBST(new TreeNode(5)));
+      //  System.out.println(new DecChallenge().isValidBST(new TreeNode(5)));
+        System.out.println(new DecChallenge().decodeAtIndex("leet2code3", 15));
+    }
+
+
+    public String decodeAtIndex(String S, int K) {
+
+        long size = 0;
+        int N = S.length();
+
+        // Find size = length of decoded string
+        for (int i = 0; i < N; ++i) {
+            char c = S.charAt(i);
+            if (Character.isDigit(c))
+                size *= c - '0';
+            else
+                size++;
+        }
+        for (int i = N-1; i >= 0; --i) {
+            char c = S.charAt(i);
+            K %= size;
+            if (K == 0 && Character.isLetter(c))
+                return Character.toString(c);
+
+            if (Character.isDigit(c))
+                size /= c - '0';
+            else
+                size--;
+        }
+
+        throw null;
     }
 
     public boolean isValidBST(TreeNode root) {
