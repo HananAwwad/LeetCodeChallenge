e1a4518b96ae0cacc64dfb0f790834b7a6b1f6f2
HananAwwad <eng.hanan.awwad@gmail.com>
2021-06-22 21:49:11 +0300

    Contiguous Subarrays

--- src/com/hanan/interviews/FacebookInterview.java
+++ src/com/hanan/interviews/FacebookInterview.java
@@ -3,12 +3,39 @@ package com.hanan.interviews;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Stack;
 
 public class FacebookInterview {
     public static void main(String[] a) {
         // aVeryBigSum(new ArrayList<Long>(){1000, 100});
         ///  System.out.println(new FacebookInterview().spiralOrder(new int[][]{}));
-        System.out.println(new FacebookInterview().rotationalCipher("abcdefghijklmNOPQRSTUVWXYZ0123456789", 39));
+        //System.out.println(new FacebookInterview().rotationalCipher("abcdefghijklmNOPQRSTUVWXYZ0123456789", 39));
+        System.out.println(new FacebookInterview().countSubarrays(new int[]{3, 4, 1, 6, 2}));
+    }
+
+    int[] countSubarrays(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        int[] ans = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                int idx = stack.pop();
+                ans[i] += ans[idx];
+            }
+            stack.push(i);
+            ans[i]++;
+        }
+        stack.clear();
+        int[] temp = new int[arr.length];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                int idx = stack.pop();
+                ans[i] += temp[idx];
+                temp[i] += temp[idx];
+            }
+            stack.push(i);
+            temp[i]++;
+        }
+        return ans;
     }
 
     String rotationalCipher(String input, int rotationFactor) {
